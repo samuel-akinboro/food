193a6d7813dd425058001711e826927e8f16e247
Samuel Akinboro <victorakinboro2003@gmail.com>
2022-05-29 17:22:03 +0100

    project initialization with navigation

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const SearchScreen = () => {
+  return (
+    <View>
+      <Text>Welcome to Foodie</Text>
+    </View>
+  )
+}
+
+export default SearchScreen
