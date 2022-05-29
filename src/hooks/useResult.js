0a9016b2a7401dd02d0fe56d230f9b3ecf367ff1
Samuel Akinboro <victorakinboro2003@gmail.com>
2022-05-29 23:29:49 +0100

    introduced hooks

---
+++ src/hooks/useResult.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import yelp from '../api/yelp'
+
+export default () => {
+  const [searchResult, setSearchResult] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  useEffect(()=>{
+    handleSearchApi('pasta')
+  }, [])
+
+  const handleSearchApi = async (searchParam) => {
+    try {
+      const response = yelp.get('/search', {
+        params: {
+          term: searchParam,
+          location: 'san jose',
+          limit: 50
+        }
+      })
+  
+      setSearchResult((await response).data.businesses)
+    } catch (err){
+      setErrorMessage('Something went wrong.')
+    }
+  }
+
+  return {
+    searchResult,
+    errorMessage,
+    handleSearchApi
+  }
+}
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,31 +1,20 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import SearchBar from '../components/SearchBar'
-import yelp from '../api/yelp'
+import useResult from '../hooks/useResult'
 
 const SearchScreen = () => {
+  const { errorMessage, handleSearchApi, searchResult } = useResult();
   const [searchTerm, setSearchTerm] = React.useState('');
-  const [searchResult, setSearchResult] = React.useState([]);
-
-  const handleSearchApi = async () => {
-    const response = yelp.get('/search', {
-      params: {
-        term: searchTerm,
-        location: 'san jose',
-        limit: 50
-      }
-    })
-
-    setSearchResult((await response).data.businesses)
-  }
 
   return (
     <View style={styles.containerStyle}>
       <SearchBar
         searchTerm={searchTerm}
         setSearchTerm={setSearchTerm}
-        onTermSubmit={handleSearchApi}
+        onTermSubmit={() => handleSearchApi(searchTerm)}
       />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>Welcome to Foodie, There are a total of {searchResult.length} restaurants</Text>
     </View>
   )
